3ad9a35d9697952f0d88d785077014238ae92fc0
Nichiporchik-Nastya <nai200315@gmail.com>
2024-02-19 00:50:38 +0300

    update: burger menu

--- js/anchorScroll.js
+++ js/anchorScroll.js
@@ -1,21 +1,28 @@
+// let burgerMenu = document.querySelector(".burger-menu");
+
 function scrollToBlock(event) {
-    event.preventDefault(); // Отменяем стандартное поведение ссылки
-  
-    const targetElement = document.querySelector(
-      event.target.getAttribute('href')
-    ); // Получаем элемент, к которому нужно пролистать
-    const offset = targetElement.offsetTop - 80; // Считаем позицию с учетом смещения на 50px
-  
-    window.scrollTo({
-      top: offset,
-      behavior: 'smooth',
-    });
+
+  if (burgerMenu.classList.contains("menu-active")) {
+    burgerMenu.classList.remove("menu-active");
   }
-  
-  // Получаем все ссылки, к которым есть якорь
-  const linksWithAnchor = document.querySelectorAll('a[href^="#"]');
-  
-  // Добавляем обработчик события для каждой ссылки
-  linksWithAnchor.forEach((link) => {
-    link.addEventListener('click', scrollToBlock);
+
+  event.preventDefault(); // Отменяем стандартное поведение ссылки
+
+  const targetElement = document.querySelector(
+    event.target.getAttribute('href')
+  ); // Получаем элемент, к которому нужно пролистать
+  const offset = targetElement.offsetTop - 80; // Считаем позицию с учетом смещения на 50px
+
+  window.scrollTo({
+    top: offset,
+    behavior: 'smooth',
   });
+}
+
+// Получаем все ссылки, к которым есть якорь
+const linksWithAnchor = document.querySelectorAll('a[href^="#"]');
+
+// Добавляем обработчик события для каждой ссылки
+linksWithAnchor.forEach((link) => {
+  link.addEventListener('click', scrollToBlock);
+});
--- js/form.js
+++ js/form.js
@@ -2,7 +2,6 @@ let checkbox = document.querySelector(".form-ckbox"),
 label = document.querySelector("label[for='ckbox']");
 
 if (checkbox.checked) {
-    
     label.classList.add("unchecked");
 }
 
