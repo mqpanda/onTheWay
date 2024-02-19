c2a0e9884cf56d26394c37f007e57449671a584d
Nichiporchik-Nastya <nai200315@gmail.com>
2024-02-19 12:40:44 +0300

    feat: modal window

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,45 +1,20 @@
 let modal = document.body.querySelector(".modal_wrapper"),
-    modalTitle = document.body.querySelector(".modal_title"),
-    modalOldPrice = document.body.querySelector(".modal_block__old-price"),
-    modalNewPrice = document.body.querySelector(".modal_block__price"),
-    modalInfo = document.body.querySelectorAll(".modal_info"),
-    modalImg = document.body.querySelector(".modal_img"),
-    modalLikes = document.body.querySelector(".modal_likes"),
-    modalCardBottom = document.body.querySelector(".modal_block__bottom"); let modalCloseButton = document.body.querySelector(".modal_close_button");
+    modalCloseButton = document.body.querySelector(".modal_close_button"),
+    submitBtn = document.querySelector("#form-block")
+
+    submitBtn.addEventListener("click", ()=>{
+        toggleModal();
+        StopScroll();
+    });
+
 if (modalCloseButton) {
     modalCloseButton.addEventListener("click", closeModal);
 }
-let ids = document.body.querySelectorAll("div[data-product]");
-ids.forEach(card => {
-    if (card.getBoundingClientRect().left > window.innerWidth) {
-        card.classList.remove("animated-item");
-    }
-    card.addEventListener("click", () => {
-        toggleModal(card);
-    });
-});
-function toggleModal(card) {
-    modalTitle.innerHTML = card.querySelector(".card__title").innerHTML;
-    modalInfo[0].innerHTML = card.querySelectorAll(".card__info")[0].innerHTML;
-    modalInfo[1].innerHTML = card.querySelectorAll(".card__info")[1].innerHTML;
-    price = parseFloat(card.querySelector(".card__price").innerHTML.slice(0, -4)); modalOldPrice.innerHTML = price + " руб.";
-    modalNewPrice.innerHTML = +price - (+price * 15 * 0.01) + " руб.";
-    modalImg.src = card.querySelector(".card__img").src;
-    modalLikes.querySelector(".card__rating-number").innerHTML = card.querySelector(".card__rating-number").innerHTML; if (card.querySelector(".likes__like").classList.contains("clicked")) {
-        modalLikes.querySelector(".likes__like").classList.add("clicked");
-    }
-    else {
-        modalLikes.querySelector(".likes__like").classList.remove("clicked");
-    }
-    if (card.querySelector(".card__cart-btn").classList.contains("card__cart-btn_active")) {
-        modalCardBottom.querySelector(".card__cart-btn").classList.add("card__cart-btn_active");
-        modalCardBottom.querySelector(".card__cart-btn").innerHTML = "В корзине &#10004;";
-    }
-    else {
-        modalCardBottom.querySelector(".card__cart-btn").classList.remove("card__cart-btn_active");
-        modalCardBottom.querySelector(".card__cart-btn").innerHTML = "В корзину";
-    }
 
+
+
+
+function toggleModal() {
     modal.classList.toggle("is-open");
     window.addEventListener("scroll", StopScroll);
 };
