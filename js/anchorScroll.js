5d60be689470fc6883a008d0572bb60e3c4556b0
Nichiporchik-Nastya <nai200315@gmail.com>
2024-02-19 11:06:45 +0300

    update: links

--- js/anchorScroll.js
+++ js/anchorScroll.js
@@ -1,7 +1,4 @@
-// let burgerMenu = document.querySelector(".burger-menu");
-
-function scrollToBlock(event) {
-
+function scrollToAbout(event) {
   if (burgerMenu.classList.contains("menu-active")) {
     burgerMenu.classList.remove("menu-active");
   }
@@ -24,5 +21,36 @@ const linksWithAnchor = document.querySelectorAll('a[href^="#"]');
 
 // Добавляем обработчик события для каждой ссылки
 linksWithAnchor.forEach((link) => {
-  link.addEventListener('click', scrollToBlock);
+  link.addEventListener('click', scrollToAbout);
 });
+
+
+/*
+function scrollToBlock(event) {
+  // event.stopPropagation()
+  
+
+  let href = event.target.getAttribute('href');
+  console.log(href);
+  const targetElement = document.querySelector(href); // Получаем элемент, к которому нужно пролистать
+  targetElement.scrollIntoView({
+    top: 100,
+    block: "start",
+    behavior: 'smooth',
+  })
+  let offset = targetElement.offsetTop + 80; // Считаем позицию с учетом смещения на 50px
+
+  // if (href == "#form-block") {
+  //   offset = targetElement.offsetTop - 300;
+  //   console.log(1234);
+  // }
+
+}
+
+// Получаем все ссылки, к которым есть якорь
+const linksWithAnchor = document.querySelectorAll('a[href^="#"]');
+
+// Добавляем обработчик события для каждой ссылки
+linksWithAnchor.forEach((link) => {
+  link.addEventListener('click', scrollToBlock);
+});*/
